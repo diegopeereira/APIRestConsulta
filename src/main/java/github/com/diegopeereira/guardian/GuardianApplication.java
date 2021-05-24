7695749b052daa508bd04f5d751f0dc9298dd33c
Diego Pereira <diego_tlb@hotmail.com>
2021-05-24 17:39:45 -0300

    API Rest Consulta

---
+++ src/main/java/github/com/diegopeereira/guardian/GuardianApplication.java
@@ -0,0 +1,13 @@
+package github.com.diegopeereira.guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardianApplication.class, args);
+	}
+
+}
